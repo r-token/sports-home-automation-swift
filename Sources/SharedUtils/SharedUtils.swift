484ca31f8839b1036e296567dff64bd284560413
Ryan Token <ryantoken13@gmail.com>
2025-02-08 14:15:49 -0700

    Comments

--- Sources/SharedUtils/SharedUtils.swift
+++ Sources/SharedUtils/SharedUtils.swift
@@ -8,5 +8,8 @@
 import Extensions
 import Foundation
 
+// Football season = August through February
 public let isFootballSeason = (8...12).contains(Date().month) || (1...2).contains(Date().month)
+
+// Basketball season = October through April 15
 public let isBasketballSeason = (10...12).contains(Date().month) || (1...4).contains(Date().month) && !(Date().month == 4 && Date().day > 15)
